b31fd0f6f4f5e3802a892b65c1a4c167a4328ea6
Sanji Winaya <sanzlvr654@gmail.com>
2023-02-20 23:07:50 +0700

    v1 push

---
+++ include/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Page Masyarakat</title>
+    <link rel="stylesheet" href="https:
+  <!-- Font Awesome Icons -->
+  <link rel=" stylesheet" href="../assets/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="../assets/dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition layout-top-nav">
+    <div class="wrapper">
