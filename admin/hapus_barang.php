6573f78d949df7fe8906bf569b7f005a9c6f6855
Michael Sanji <akunbotak503@gmail.com>
2023-02-23 11:41:18 +0700

    perbaiki landing page sistem

--- admin/hapus_barang.php
+++ admin/hapus_barang.php
@@ -5,7 +5,6 @@ include "../db/dbconnect.php";
 // menangkap data id yang di kirim dari url
 $id_barang = $_GET['id_barang'];
 
-
 // menghapus data dari database
 mysqli_query($conn,"delete from tb_barang where id_barang='$id_barang'");
 
---
+++ include/nav.php
@@ -0,0 +1,15 @@
+<nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Beranda</a></li>
+          <li><a class="nav-link scrollto" href="#about">Tentang Kami</a></li>
+          <li><a class="nav-link scrollto" href="#services">Keunggulan Kami</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Barang Lelang</a></li>
+          <li><a class="nav-link scrollto" href="#testimonials">Testimoni Pelanggan</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Kontak</a></li>
+          <li><a class="getstarted scrollto" href="user/login.php">LOGIN</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
