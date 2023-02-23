e2f246a34f9b4288932354b4d7ef09b8abb04bbd
Michael Sanji <akunbotak503@gmail.com>
2023-02-23 08:43:28 +0700

    fix pw db host

--- db/dbconnect.php
+++ db/dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "aratade1_sanjifxt", "cilengkrang2", "aratade1_elelang");
+$conn = mysqli_connect("localhost", "aratade1_sanjifxt", "#cilengkrang2", "aratade1_elelang");
 
 if (mysqli_connect_errno()) {
     echo 'Koneksi Database Gagal : ' . mysqli_connect_error();
