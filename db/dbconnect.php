2f1710c64f030a6ed8524747fa4d6c4b5bf36cdf
Michael Sanji <akunbotak503@gmail.com>
2023-02-23 12:26:51 +0700

    db

--- db/dbconnect.php
+++ db/dbconnect.php
@@ -1,8 +1,7 @@
 <?php
-// $conn = mysqli_connect("localhost", "aratade1_sanjifxt", "#cilengkrang2", "aratade1_elelang");
-$conn = mysqli_connect("localhost", "root", "", "elelang");
+$conn = mysqli_connect("localhost", "aratade1_sanjifxt", "#cilengkrang2", "aratade1_elelang");
+// $conn = mysqli_connect("localhost", "root", "", "elelang");
 
 if (mysqli_connect_errno()) {
     echo 'Koneksi Database Gagal : ' . mysqli_connect_error();
 }
-?>
